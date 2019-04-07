4313f89e28bb2af5c9901959418d80bb3e7f98a8
Jeremy Monroe <jereamon@gmail.com>
2019-04-07 15:45:31 -0700

    Just starting to write integration test.

--- students/jeremy_monroe/lesson01/assignment/tests/test_integration.py
+++ students/jeremy_monroe/lesson01/assignment/tests/test_integration.py
@@ -1,3 +1,17 @@
 """ Integration tests for inventory_management. """
 
+import unittest
+from unittest.mock import MagicMock, patch
+import sys
 import main
+
+class InventoryIntegrationTest(unittest.TestCase):
+    """ Integration test for inventory_management. """
+
+    def test_integration_one(self):
+        """ The main test. """
+        
+        main.FULL_INVENTORY = {}
+
+        user_inputs = (user_input for user_input in ['1', 147, 'Chair', 1250, y, 'wood', 'l', '2', 147, 'q']
+
