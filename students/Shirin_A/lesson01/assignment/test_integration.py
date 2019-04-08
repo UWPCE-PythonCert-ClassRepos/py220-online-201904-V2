39c3c63a6db173d49e3d3139a479b455ca9bdd03
Mahmood08 <sirin_ruet@yahoo.com>
2019-04-08 13:55:30 -0700

    added test files

---
+++ students/Shirin_A/lesson01/assignment/test_integration.py
@@ -0,0 +1,53 @@
+"""Creates a full suite of tests for all classes in inventory_management"""
+
+from unittest.mock import patch
+from unittest import TestCase
+import sys
+sys.path.append(r"C:\Users\Public\py220-online-201904-V2\students\Shirin_A\lesson01\assignment\inventory_management")
+from electric_appliances import ElectricAppliances
+from furniture_class import Furniture
+from inventory_class import Inventory
+import market_prices
+from main import add_new_item, return_inventory
+
+
+class IntegrationTest(TestCase):
+
+    """Creates a class that tests inventory_management together"""
+
+    def test_main_integration(self):
+
+        """Test all functions """
+        price = market_prices.get_latest_price(0)
+
+        #Adding non categorized inventory item with main
+
+        input1 = ['1', 'shoe', '1', 'n', 'n']
+        item1 = Inventory('1', 'shoe', price, '1')
+        with patch('builtins.input', side_effect=input1):
+            add_new_item()
+                            
+        #Adding furniture item with main
+            
+        input2 = ['2', 'chair', '2', 'y', 'wood', 'S']        
+        item2 = Furniture('2', 'chair', price, '2', 'wood', 'S')
+        with patch('builtins.input', side_effect=input2):
+            add_new_item()
+          
+        #Adding electric appliance with main
+
+        input3 = ['3', 'stove', '3', 'n', 'y', 'LG', '100']
+        item3 = ElectricAppliances('3', 'stove', price, '3', 'LG', '100')
+        with patch('builtins.input', side_effect=input3):
+            add_new_item()
+                
+        actual_inventory = return_inventory()
+        expected_inventory = {
+
+            '1': item1.return_as_dictionary(),
+
+            '2': item2.return_as_dictionary(),
+
+            '3': item3.return_as_dictionary()}
+
+        self.assertEqual(actual_inventory, expected_inventory)
