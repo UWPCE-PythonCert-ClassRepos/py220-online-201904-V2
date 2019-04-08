c702df23071dbb338e85b9092e8330f0399f62a9
Jeremy Monroe <jereamon@gmail.com>
2019-04-08 13:55:05 -0700

    Adding my class_notes so I can keep it synched across computers.

---
+++ students/jeremy_monroe/lesson02/activity/simple.py
@@ -0,0 +1,18 @@
+# simple.py
+
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+def my_fun(n):
+    for i in range(0, n):
+        logging.debug(i)
+        if i == 50:
+            logging.warning('The value of i is 50.')
+        try:
+            100 / (50 - i)
+        except ZeroDivisionError:
+            logging.error("Tried to divide by zero. Var i was {}. Recovered gracefully.".format(i))
+
+if __name__ == "__main__":
+    my_fun(100)
+
