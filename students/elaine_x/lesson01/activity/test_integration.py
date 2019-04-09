ffc6820689bc874d7a81c287e8907ee50309be96
yiyunxu <44690219+yiyunxu@users.noreply.github.com>
2019-04-08 20:49:17 -0700

    submit

---
+++ students/elaine_x/lesson01/activity/test_integration.py
@@ -0,0 +1,36 @@
+'''mod string'''
+from unittest import TestCase
+# from unittest.mock import MagicMock
+
+from calculator.adder import Adder
+from calculator.subtracter import Subtracter
+from calculator.multiplier import Multiplier
+from calculator.divider import Divider
+from calculator.calculator import Calculator
+# from calculator.exceptions import InsufficientOperands
+
+
+class ModuleTests(TestCase):
+    '''doc string'''
+    def test_module(self):
+        '''doc string'''
+        calculator = Calculator(Adder(), Subtracter(), Multiplier(), Divider())
+
+        calculator.enter_number(5)
+        calculator.enter_number(2)
+
+        calculator.multiply()
+
+        calculator.enter_number(46)
+
+        calculator.add()
+
+        calculator.enter_number(56)
+
+        calculator.divide()
+
+        calculator.enter_number(1)
+
+        result = calculator.subtract()
+
+        self.assertEqual(0, result)
