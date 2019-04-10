f5b010aca8f1ebd012cb3413aa95608a665d201a
Douglas Klos <dougklos@gmail.com>
2019-04-09 17:47:39 -0700

    Pylint 10 w/o disables, formatted, and working

--- students/douglas_klos/lesson02/assignment/src/charges_calc.py
+++ students/douglas_klos/lesson02/assignment/src/charges_calc.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 """Returns total price paid for individual rentals"""
+
+# Douglas Klos
+# April 9th, 2019
+# Python 220
+# Lesson 02, debugging and logging
+
 import argparse
 import json
 import datetime
@@ -7,98 +13,187 @@ import math
 import logging
 
 
-log_format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
-log_file = datetime.datetime.now().strftime("%Y-%m-%d")+'.log'
+LOG_FILE = datetime.datetime.now().strftime("%Y-%m-%d") + ".log"
+LOG_FORMAT = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+FORMATTER = logging.Formatter(LOG_FORMAT)
 
-formatter = logging.Formatter(log_format)
+FILE_HANDLER = logging.FileHandler(LOG_FILE)
+FILE_HANDLER.setLevel(logging.DEBUG)
+FILE_HANDLER.setFormatter(FORMATTER)
 
-file_handler = logging.FileHandler(log_file)
-file_handler.setLevel(logging.WARNING)
-file_handler.setFormatter(formatter)
+CONSOLER_HANDLER = logging.StreamHandler()
+CONSOLER_HANDLER.setLevel(logging.DEBUG)
+CONSOLER_HANDLER.setFormatter(FORMATTER)
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-console_handler.setFormatter(formatter)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.NOTSET)
+LOGGER.addHandler(FILE_HANDLER)
+LOGGER.addHandler(CONSOLER_HANDLER)
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+LOGGER.disabled = True
+
+
+def main():
+    """main function"""
+    args = parse_cmd_arguments()
+    set_debug_level(args)
+    data = load_rentals_file(args.input)
+    data = repair_dates(data)
+    data, bad_data = calculate_additional_fields(data)
+    data = remove_bad_data(data, bad_data)
+    save_to_json(args.output, data)
+    save_to_json(args.output + ".bad", bad_data)
 
 
 def parse_cmd_arguments():
-    logging.info('Start of parse_cmd_arguments()')
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-i', '--input', help='input JSON file', required=True)
-    parser.add_argument('-o', '--output', help='ouput JSON file', required=True)
-    parser.add_argument('-d', '--debug', help='debugger level', required=False)
+    """Parses the command line arguments
 
+    Returns:
+        ArgumentParser.parse_args
+
+    """
+    logging.info("Start of parse_cmd_arguments()")  # We'll never see this
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument("-i", "--input", help="input JSON file", required=True)
+    parser.add_argument("-o", "--output", help="ouput JSON file", required=True)
+    parser.add_argument("-d", "--debug", help="debugger level", required=False)
     return parser.parse_args()
 
 
+def set_debug_level(args):
+    """Sets the debug level based on command line arguments"""
+    if args.debug:
+        if args.debug == "0":
+            pass
+        elif args.debug == "1":
+            LOGGER.disabled = False
+            LOGGER.setLevel(logging.ERROR)
+        elif args.debug == "2":
+            LOGGER.disabled = False
+            LOGGER.setLevel(logging.WARNING)
+        elif args.debug == "3":
+            LOGGER.disabled = False
+            LOGGER.setLevel(logging.DEBUG)
+        else:
+            print(f"Invalid debug level specified: {args.debug}.  (1-4)")
+            exit(0)
+
+
 def load_rentals_file(filename):
-    logging.info('-----Start of load_rental_file-----')
+    """Loads rental data from input json file
+
+    Args:
+        filename: Filename to read data from
+
+    Returns:
+        dict: Dictionary of rental data
+
+    """
+    logging.info("-----Start of load_rental_file-----")
     with open(filename) as file:
         try:
-            data = json.load(file)
-        except Exception as e:
-            logging.critical(f'Loading data from json failed.\n\tException: {repr(e)}')
+            new_data = json.load(file)
+        except json.decoder.JSONDecodeError as ex:
+            logging.critical(
+                "Loading data from json failed.\n\tException: %s", repr(ex)
+            )
             exit(0)
-    return data
+    return new_data
+
 
 def repair_dates(data):
-    logging.info('-----Start of repair_dates-----')
+    """Repairs incorrect dates in data
+
+    Args:
+        input_data (dict): Dictionary of data to be parsed
+
+    Returns:
+        dict: Dictionary with dates repaired
+
+    """
+    logging.info("-----Start of repair_dates-----")
     for key, value in data.items():
         try:
-            rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
-            rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
+            rental_start = datetime.datetime.strptime(
+                value["rental_start"], "%m/%d/%y"
+            )
+            rental_end = datetime.datetime.strptime(
+                value["rental_end"], "%m/%d/%y"
+            )
             if rental_end < rental_start:
-                logging.warning(f'Key:{key} rental start, end dates reversed.  Repairing.')
-                value['rental_start'], value['rental_end'] = value['rental_end'], value['rental_start']
+                logging.warning(
+                    "Key:%s rental start, end dates reversed.  Repairing.", key
+                )
+                value["rental_start"], value["rental_end"] = (
+                    value["rental_end"],
+                    value["rental_start"],
+                )
         except ValueError:
-            logging.error(f'Key:{key} contains bad date data.  Skipping.')
-            logging.debug(f'Value:{value}')
-
+            logging.error("Key:%s contains bad date data.  Skipping.", key)
+            logging.debug("Value:%s", value)
     return data
 
 
-def calculate_additional_fields(data, bad_data):
-    logging.info('-----Start of load_additional_fields-----')
+def calculate_additional_fields(data):
+    """Calculates additional fields of data
+
+    Args:
+        input_data (dict): Dictionary of data to be parsed
+
+    Returns:
+        dict, dict: good_data, bad_data
+
+    """
+    bad_data = {}
+    logging.info("-----Start of load_additional_fields-----")
     for key, value in data.items():
         try:
-            rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
-            rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
-            value['total_days'] = (rental_end - rental_start).days
-            value['total_price'] = value['total_days'] * value['price_per_day']
-            value['sqrt_total_price'] = math.sqrt(value['total_price'])
-            value['unit_cost'] = value['total_price'] / value['units_rented']
-        except Exception as e:
+            rental_start = datetime.datetime.strptime(
+                value["rental_start"], "%m/%d/%y"
+            )
+            rental_end = datetime.datetime.strptime(
+                value["rental_end"], "%m/%d/%y"
+            )
+            value["total_days"] = (rental_end - rental_start).days
+            value["total_price"] = value["total_days"] * value["price_per_day"]
+            value["sqrt_total_price"] = math.sqrt(value["total_price"])
+            value["unit_cost"] = value["total_price"] / value["units_rented"]
+        except (ZeroDivisionError, ValueError):
             bad_data[key] = value
-            logging.error(f'Key:{key} Failed calculating additional fields.  Skipping.')
-            logging.debug(f'Value:{value}')
-
+            logging.error(
+                "Key:%s Failed calculating additional fields.  Skipping.", key
+            )
+            logging.debug("Value:%s", value)
     return data, bad_data
 
 
 def remove_bad_data(data, bad_data):
+    """Removes bad entries from the dictionary
+
+    Args:
+        input_data (dict): Dictionary of data to be parsed
+        input_bad_data (dict): Dictionary of bad json entries to be removed
+
+    Returns:
+        dict: Dictionary of valid entries
+
+    """
     for key in bad_data:
         del data[key]
-    
-    return data, bad_data
+    return data
 
 
 def save_to_json(filename, data):
-    logging.info('-----Start of save_to_json(filename, data)-----')
-    with open(filename, 'w') as file:
+    """Saves the input_data to filename in json format
+
+    Args:
+        input_data (dict): Dictionary of data to be saved
+
+    """
+    logging.info("-----Start of save_to_json(%s, data)-----", filename)
+    with open(filename, "w") as file:
         json.dump(data, file)
 
 
 if __name__ == "__main__":
-    bad_data = {}
-    args = parse_cmd_arguments()
-    data = load_rentals_file(args.input)
-    data = repair_dates(data)
-    data, bad_data = calculate_additional_fields(data, bad_data)
-    data, bad_data = remove_bad_data(data, bad_data)
-    save_to_json(args.output, data)
-    save_to_json('bad_data.json', bad_data)
+    main()
