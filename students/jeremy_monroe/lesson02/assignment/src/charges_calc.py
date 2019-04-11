0bc37f2bbcaa5f16cd5cc440be8de20c1ebad434
Jeremy Monroe <jereamon@gmail.com>
2019-04-11 15:21:11 -0700

    Ok, I understand how the program flows and what is causing errors. I have console logging message in place and will get logging to a file set up when I return.

--- students/jeremy_monroe/lesson02/assignment/src/charges_calc.py
+++ students/jeremy_monroe/lesson02/assignment/src/charges_calc.py
@@ -22,7 +22,8 @@ def parse_cmd_arguments():
 
 def turn_on_debug(debug_level):
     debug_levels = {'1': logging.ERROR, '2': logging.WARNING, '3': logging.DEBUG}
-    log_format  = "%(levelname)s %(asctime)s %(filename)s:%(lineno)-4d \n%(message)s"
+    log_format  = ("\n%(levelname)s %(asctime)s"
+    "%(filename)s:%(lineno)-4d\n%(message)s")
 
     formatter = logging.Formatter(log_format)
 
@@ -40,32 +41,43 @@ def load_rentals_file(filename):
         # Ok, so there's a typo in the source.json file. Need to set up a
         # logging message that will inform the user of the error if they have
         # debug messages turned on and without crashing the program.
+        logging.debug("At start of load_rentals_file.")
         try:
             data = json.load(file)
 
         # It took me a while but I figured out how to access the error's
         # attributes.
         except json.JSONDecodeError as json_error: 
-            logging.error(("Error processing json file.\n"
+            logging.error(("Critical error unable to load file: {}\n"
+                "This error needs to be fixed before any data can be processed.\n"
                 "Json error message: {}\n"
                 "Line Number: {}\n"
-                "Column Number: {}").format(json_error.msg, json_error.lineno, json_error.colno))
+                "Column Number: {}").format(filename, json_error.msg, json_error.lineno, json_error.colno))
             exit(1)
 
     return data
 
 def calculate_additional_fields(data):
-    print("in first line of calculate_additional_fields")
-    for value in data.values():
+    logging.debug("At start of calculate_additional_fields")
+    for key, value in data.items():
         try:
             rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
             rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
+            # Ok, so sometimes the rental_start and end are mixed up.
             value['total_days'] = (rental_end - rental_start).days
             value['total_price'] = value['total_days'] * value['price_per_day']
+            # I'm not sure why we need the sqrt of total price... But it is
+            # throwing an error since our total_price was negative.
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
             value['unit_cost'] = value['total_price'] / value['units_rented']
-        except:
-            exit(0)
+        except ValueError as value_error:
+            logging.warning(("\nError in input file: {}\n"
+            "Program will continue and process all unaffected data.\n"
+            "Item with error will be included in output but will "
+            "have errors or missing data. Fix the error in the "
+            "source file and rerun to fix errors in output.\n"
+            "Item number: {}\n"
+            "Item info:\n{}").format(args.input, key, value))
 
     return data
 
--- students/jeremy_monroe/lesson02/assignment/src/charges_calc_debugging_notes.md
+++ students/jeremy_monroe/lesson02/assignment/src/charges_calc_debugging_notes.md
@@ -48,3 +48,92 @@ python3 -m pdb charges_calc.py --input=source.json --output=test_output.json -d=
 > '1'
 ```
 
+
+
+Ok, I've implemented an error message for buggy json imports and have moved on to  `calculate_additional_fields`.  By debugging I discovered that `total_days` was resulting in a negative number of days and subsequently throwing an error when calculating `sqrt_total_price`.
+
+This is me later. Looks like it's not `rental_end - rental_start` that's giving me negative `total_days` but errors in the json file.
+
+```
+python3 -m pdb charges_calc.py -i=source_bug_fixed.json -o=test_output.json -d=1
+
+-> '''
+(Pdb) b 59
+Breakpoint 1 at charges_calc.py:59
+(Pdb) c
+-> print("in first line of calculate_additional_fields")
+(Pdb) n
+in first line of calculate_additional_fields
+-> for value in data.values():
+(Pdb) n
+-> try:
+(Pdb) n
+-> rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
+(Pdb) pp value['rental_start']
+'6/12/17'
+(Pdb) n
+-> rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
+(Pdb) pp rental_start
+datetime.datetime(2017, 6, 12, 0, 0)
+(Pdb) n
+-> value['total_days'] = (rental_end - rental_start).days
+(Pdb) n
+-> value['total_price'] = value['total_days'] * value['price_per_day']
+(Pdb) pp value['total_days']
+-82
+(Pdb) n
+-> value['sqrt_total_price'] = math.sqrt(value['total_price'])
+(Pdb) pp value['price_per_day']
+31
+(Pdb) pp rental_start - rental_end
+datetime.timedelta(82)
+(Pdb) pp (rental_start - rental_end).days
+82
+(Pdb) n
+ValueError: math domain error
+-> value['sqrt_total_price'] = math.sqrt(value['total_price'])
+(Pdb) pp value['total_price']
+-2542
+(Pdb) n
+-> except:
+(Pdb) pp value['unit_cost']
+*** KeyError: 'unit_cost'
+(Pdb) pp value['total_price'] / value['units_rented']
+-317.75
+(Pdb) pp value['sqrt_total_price']
+*** KeyError: 'sqrt_total_price'
+(Pdb) pp value['total_price']
+-2542
+(Pdb) pp math.sqrt(value['total_price'])
+*** ValueError: math domain error
+
+```
+
+
+
+I changed the for loop at the start of `calculate_additional_fields()` to `for key, value in data.items()` so that I can use the `key` in the logging message to help users find and correct errors in the json file.
+
+```
+python3 -m pdb charges_calc.py -i=source_bug_fixed.json -o=test_output.json -d=1
+
+-> '''
+(Pdb) b 59
+Breakpoint 1 at charges_calc.py:59
+(Pdb) c
+-> print("in first line of calculate_additional_fields")
+(Pdb) b 74
+Breakpoint 2 at charges_calc.py:74
+(Pdb) c
+in first line of calculate_additional_fields
+-> print(value)
+(Pdb) pp key
+'RNT776'
+(Pdb) pp value
+{'price_per_day': 14,
+ 'product_code': 'PRD70',
+ 'rental_end': '',
+ 'rental_start': '6/19/16',
+ 'units_rented': 1}
+(Pdb)
+```
+
