8c3337397f2b4c4e62b936737321a2761609371a
Rachel Schirra <schirra.rachel@gmail.com>
2019-04-13 13:08:24 -0700

    Linted it.

--- students/rachel_s/lesson02/assignment/src/charges_calc.py
+++ students/rachel_s/lesson02/assignment/src/charges_calc.py
@@ -1,12 +1,17 @@
 '''
-Returns total price paid for individual rentals 
+Returns total price paid for individual rentals
 '''
+
 import argparse
 import json
 import datetime
 import math
 
 def parse_cmd_arguments():
+    """
+    Takes command arguments and returns arguments as parsed according to
+    the JSON file
+    """
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i', '--input', help='input JSON file', required=True)
     parser.add_argument('-o', '--output', help='ouput JSON file', required=True)
@@ -15,33 +20,49 @@ def parse_cmd_arguments():
 
 
 def load_rentals_file(filename):
+    """
+    Loads rentals JSON file and exits with a FileNotFoundError if it
+    encounters a problem.
+    """
     with open(filename) as file:
         try:
-            data = json.load(file)
-        except:
+            rentals = json.load(file)
+        except FileNotFoundError:
             exit(0)
-    return data
+    return rentals
 
-def calculate_additional_fields(data):
-    for value in data.values():
+def calculate_additional_fields(rental_data):
+    """
+    Accepts a rental_data JSON file.
+    Calculates total days of rental, total price, and unit cost. Exits
+    if a key is not found in rental_data.
+    """
+    for value in rental_data.values():
         try:
-            rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
-            rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
+            rental_start = datetime.datetime.strptime(
+                value['rental_start'],
+                '%m/%d/%y')
+            rental_end = datetime.datetime.strptime(
+                value['rental_end'],
+                '%m/%d/%y')
             value['total_days'] = (rental_end - rental_start).days
             value['total_price'] = value['total_days'] * value['price_per_day']
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
             value['unit_cost'] = value['total_price'] / value['units_rented']
-        except:
+        except KeyError:
             exit(0)
 
-    return data
+    return rental_data
 
-def save_to_json(filename, data):
+def save_to_json(filename, rental_data):
+    """
+    Saves modified rental data as JSON with the specified file name.
+    """
     with open(filename, 'w') as file:
-        json.dump(data, file)
+        json.dump(rental_data, file)
 
 if __name__ == "__main__":
-    args = parse_cmd_arguments()
-    data = load_rentals_file(args.input)
-    data = calculate_additional_fields(data)
-    save_to_json(args.output, data)
+    __args__ = parse_cmd_arguments()
+    __data__ = load_rentals_file(__args__.input)
+    __data__ = calculate_additional_fields(__data__)
+    save_to_json(__args__.output, __data__)
