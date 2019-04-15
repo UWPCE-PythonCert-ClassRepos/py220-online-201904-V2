b0ab20603accef7875f73a22e54a4e292a77e7be
Douglas Klos <dougklos@gmail.com>
2019-04-15 00:18:10 -0700

    l03 initializing database with sqlalchemy

--- students/douglas_klos/lesson03/assignment/sqlalchemy/tests/test_gradel03.py
+++ students/douglas_klos/lesson03/assignment/sqlalchemy/tests/test_gradel03.py
@@ -1,3 +1,4 @@
+# pylint: disable=C0111, R0801
 """
     Autograde Lesson 3 assignment
     Run pytest
@@ -6,21 +7,21 @@
 
 """
 
-import pytest
 
-import basic_operations as l
+import pytest
+import src.basic_operations as l
 
 
 @pytest.fixture
 def _add_customers():
     return [
-        ("123", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("456", "Name", "Lastname", "Address", "phone", "email", "inactive", 10),
-        ("123", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("789", "Name", "Lastname", "Address", "phone", "email", "active", 0),
-        ("345", "Name", "Lastname", "Address", "phone", "email", "active", -10),
-        ("0123", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("777", "Name", "Lastname", "Address", "phone", "email", "active", 999),
+        ("123", "Name", "Lastname", "Address", "cell", "email", "active", 999),
+        ("234", "Name", "Lastname", "Address", "cell", "email", "inactive", 5),
+        ("345", "Name", "Lastname", "Address", "cell", "email", "active", 999),
+        ("456", "Name", "Lastname", "Address", "cell", "email", "active", 0),
+        ("567", "Name", "Lastname", "Address", "cell", "email", "active", -10),
+        ("678", "Name", "Lastname", "Address", "cell", "email", "active", 999),
+        ("789", "Name", "Lastname", "Address", "cell", "email", "active", 999),
     ]
 
 
@@ -28,8 +29,8 @@ def _add_customers():
 def _search_customers():  # needs to del with database
     return [
         [
-            ("998", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-            ("997", "Name", "Lastname", "Address", "phone", "email", "inactive", 10),
+            ("998", "Name", "Last", "Address", "cell", "email", "active", 999),
+            ("997", "Name", "Last", "Address", "cell", "email", "inactive", 10),
         ],
         ("998", "000"),
     ]
@@ -38,29 +39,29 @@ def _search_customers():  # needs to del with database
 @pytest.fixture
 def _delete_customers():  # needs to del with database
     return [
-        ("898", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("897", "Name", "Lastname", "Address", "phone", "email", "inactive", 10),
+        ("898", "Name", "Lastname", "Address", "cell", "email", "active", 999),
+        ("897", "Name", "Lastname", "Address", "cell", "email", "inactive", 0),
     ]
 
 
 @pytest.fixture
 def _update_customer_credit():  # needs to del with database
     return [
-        ("798", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("797", "Name", "Lastname", "Address", "phone", "email", "inactive", 10),
-        ("796", "Name", "Lastname", "Address", "phone", "email", "inactive", -99),
+        ("798", "Name", "Lastname", "Address", "cell", "email", "active", 999),
+        ("797", "Name", "Lastname", "Address", "cell", "email", "inactive", 10),
+        ("796", "Name", "Lastname", "Address", "cell", "email", "inactive", -9),
     ]
 
 
 @pytest.fixture
 def _list_active_customers():
     return [
-        ("598", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("597", "Name", "Lastname", "Address", "phone", "email", "inactive", 10),
-        ("596", "Name", "Lastname", "Address", "phone", "email", "inactive", 99),
-        ("595", "Name", "Lastname", "Address", "phone", "email", "active", 999),
-        ("594", "Name", "Lastname", "Address", "phone", "email", "active", 10),
-        ("593", "Name", "Lastname", "Address", "phone", "email", "active", 99),
+        ("598", "Name", "Lastname", "Address", "cell", "email", "active", 999),
+        ("597", "Name", "Lastname", "Address", "cell", "email", "inactive", 10),
+        ("596", "Name", "Lastname", "Address", "cell", "email", "inactive", 99),
+        ("595", "Name", "Lastname", "Address", "cell", "email", "active", 999),
+        ("594", "Name", "Lastname", "Address", "cell", "email", "active", 10),
+        ("593", "Name", "Lastname", "Address", "cell", "email", "active", 99),
     ]
 
 
@@ -77,9 +78,9 @@ def test_list_active_customers(_list_active_customers):
             customer[6],
             customer[7],
         )
-    actives = l.list_active_customers()
 
-    assert actives == 2
+    actives = l.list_active_customers()
+    assert actives == 4
 
     for customer in _list_active_customers:
         l.delete_customer(customer[0])
@@ -131,7 +132,8 @@ def test_search_customer(_search_customers):
     assert result["email"] == _search_customers[0][1][5]
     assert result["phone_number"] == _search_customers[0][1][4]
 
-    for customer in _search_customers:
+    for customer in _search_customers[0]:
+        print(customer[0])
         l.delete_customer(customer[0])
 
 
@@ -177,3 +179,6 @@ def test_update_customer_credit(_update_customer_credit):
     with pytest.raises(ValueError) as excinfo:
         l.update_customer_credit("00100", 1000)  # error
         assert "NoCustomer" in str(excinfo.value)
+
+    for customer in _update_customer_credit:
+        l.delete_customer(customer[0])
