de23aa87e5ef65f8682de1f84d36c9ea2cbf19e1
Elaine Xu <yeeyun_@hotmail.com>
2019-04-15 23:11:53 -0700

    submit lesson02 hw

--- students/elaine_x/lesson02/assignment/src/charges_calc.py
+++ students/elaine_x/lesson02/assignment/src/charges_calc.py
@@ -1,18 +1,12 @@
 '''
-<<<<<<< HEAD
 Returns total price paid for individual rentals
 '''
 #!/usr/bin/env python3
 
-=======
-Returns total price paid for individual rentals 
-'''
->>>>>>> ff1f77f678e19a269542ef82af223e77e047410e
 import argparse
 import json
 import datetime
 import math
-<<<<<<< HEAD
 import logging
 
 LOG_FILE = datetime.datetime.now().strftime('%Y-%m-%d')+'.log'
@@ -73,27 +67,10 @@ def load_rentals_file(filename):
             data = json.load(file)
         except json.decoder.JSONDecodeError:
             logging.error('json file load error')
-=======
-
-def parse_cmd_arguments():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-i', '--input', help='input JSON file', required=True)
-    parser.add_argument('-o', '--output', help='ouput JSON file', required=True)
-
-    return parser.parse_args()
-
-
-def load_rentals_file(filename):
-    with open(filename) as file:
-        try:
-            data = json.load(file)
-        except:
->>>>>>> ff1f77f678e19a269542ef82af223e77e047410e
             exit(0)
     return data
 
 def calculate_additional_fields(data):
-<<<<<<< HEAD
     '''sss'''
     for key, value in data.items():
         try:
@@ -112,30 +89,18 @@ def calculate_additional_fields(data):
                                                           '%m/%d/%y')
                 rental_end = datetime.datetime.strptime(value['rental_start'],
                                                         '%m/%d/%y')
-=======
-    for value in data.values():
-        try:
-            rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
-            rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
->>>>>>> ff1f77f678e19a269542ef82af223e77e047410e
             value['total_days'] = (rental_end - rental_start).days
             value['total_price'] = value['total_days'] * value['price_per_day']
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
             value['unit_cost'] = value['total_price'] / value['units_rented']
-<<<<<<< HEAD
         except ZeroDivisionError:
             logging.warning('For %s, units rented is 0, skip entry.', key)
             continue
 
-=======
-        except:
-            exit(0)
->>>>>>> ff1f77f678e19a269542ef82af223e77e047410e
 
     return data
 
 def save_to_json(filename, data):
-<<<<<<< HEAD
     '''write data to output file'''
     with open(filename, 'w') as file:
         json.dump(data, file)
@@ -147,13 +112,3 @@ if __name__ == "__main__":
     DATA = load_rentals_file(ARGS.input)
     DATA = calculate_additional_fields(DATA)
     save_to_json(ARGS.output, DATA)
-=======
-    with open(filename, 'w') as file:
-        json.dump(data, file)
-
-if __name__ == "__main__":
-    args = parse_cmd_arguments()
-    data = load_rentals_file(args.input)
-    data = calculate_additional_fields(data)
-    save_to_json(args.output, data)
->>>>>>> ff1f77f678e19a269542ef82af223e77e047410e
