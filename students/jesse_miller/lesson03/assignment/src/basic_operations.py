377093e2367384037b97e2da39c1041e65818f24
UncleanlyClericr <junyatwin@gmail.com>
2019-04-17 11:27:27 -0700

    First commit, setting up functions

---
+++ students/jesse_miller/lesson03/assignment/src/basic_operations.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+'''
+Lesson 3, learning database operations.
+'''
+import logging
+import peewee
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+logging.info('Starting basic operations for customer database.')
+
+def add_customer():
+    '''
+    Adding a new customer to the DB
+    '''
+    pass
+
+def delete_customer():
+    '''
+    Deleting a customer from the DB
+    '''
+    pass
+
+def search_customer():
+    '''
+    Search DB for an active customer
+    '''
+    pass
+
+def list_active_customers():
+    '''
+    List all active customers
+    '''
+    pass
+
+def update_customer_credit():
+    '''
+    Update customer credit limits
+    '''
+    pass
