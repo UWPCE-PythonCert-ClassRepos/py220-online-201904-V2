28c8bec9f7ea923eee24120526db8f45839036df
Douglas Klos <dougklos@gmail.com>
2019-05-06 19:20:30 -0700

    l06 data generator working

--- students/douglas_klos/lesson06/assignment/src/poor_perf.py
+++ students/douglas_klos/lesson06/assignment/src/poor_perf.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 poorly performing, poorly written module
 
@@ -6,14 +7,15 @@ poorly performing, poorly written module
 import datetime
 import csv
 
+
 def analyze(filename):
     start = datetime.datetime.now()
     with open(filename) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        reader = csv.reader(csvfile, delimiter=",", quotechar='"')
         new_ones = []
         for row in reader:
             lrow = list(row)
-            if lrow[5] > '00/00/2012':
+            if lrow[5] > "00/00/2012":
                 new_ones.append((lrow[5], lrow[0]))
 
         year_count = {
@@ -22,27 +24,27 @@ def analyze(filename):
             "2015": 0,
             "2016": 0,
             "2017": 0,
-            "2018": 0
+            "2018": 0,
         }
 
         for new in new_ones:
-            if new[0][6:] == '2013':
+            if new[0][6:] == "2013":
                 year_count["2013"] += 1
-            if new[0][6:] == '2014':
+            if new[0][6:] == "2014":
                 year_count["2014"] += 1
-            if new[0][6:] == '2015':
+            if new[0][6:] == "2015":
                 year_count["2015"] += 1
-            if new[0][6:] == '2016':
+            if new[0][6:] == "2016":
                 year_count["2016"] += 1
-            if new[0][6:] == '2017':
+            if new[0][6:] == "2017":
                 year_count["2017"] += 1
-            if new[0][6:] == '2018':
+            if new[0][6:] == "2018":
                 year_count["2018"] += 1
 
         print(year_count)
 
     with open(filename) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        reader = csv.reader(csvfile, delimiter=",", quotechar='"')
 
         found = 0
 
@@ -56,8 +58,10 @@ def analyze(filename):
 
     return (start, end, year_count, found)
 
+
 def main():
-    filename = "data/exercise.csv"
+    # filename = "data/exercise.csv"
+    filename = "data/dataset.csv"
     analyze(filename)
 
 
