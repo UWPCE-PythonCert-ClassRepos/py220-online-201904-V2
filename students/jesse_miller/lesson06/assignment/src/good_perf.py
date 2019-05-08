dbfb012bd906b61685c931cecab8edf5bdf1b1fe
UncleanlyClericr <junyatwin@gmail.com>
2019-05-08 14:23:31 -0700

    Finally have my csv_generator working, and a million line csv

--- students/jesse_miller/lesson06/assignment/src/good_perf.py
+++ students/jesse_miller/lesson06/assignment/src/good_perf.py
@@ -5,9 +5,15 @@ poorly performing, poorly written module
 
 import datetime
 import csv
+# from timeit import timeit
+
 
 def analyze(filename):
+    '''
+    This analyzes the file for parsing via the below rules.
+    '''
     start = datetime.datetime.now()
+
     with open(filename) as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         new_ones = []
@@ -50,6 +56,7 @@ def analyze(filename):
             lrow = list(line)
             if "ao" in line[6]:
                 found += 1
+
         print(year_count)
 
         print(f"'ao' was found {found} times")
@@ -58,6 +65,10 @@ def analyze(filename):
     return (start, end, year_count, found)
 
 def main():
+    '''
+    Here's where our filename is, it's hard coded at the moment, I'll see about
+    dynamic later.
+    '''
     filename = "data/exercise.csv"
     analyze(filename)
 
