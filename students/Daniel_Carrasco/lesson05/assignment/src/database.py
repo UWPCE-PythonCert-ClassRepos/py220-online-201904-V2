a94e6e6f0092cc57e547f75e3effe0360591e2f5
dac2229 <dac2229@gmail.com>
2019-05-13 17:20:02 -0700

    added db.log

--- students/Daniel_Carrasco/lesson05/assignment/src/database.py
+++ students/Daniel_Carrasco/lesson05/assignment/src/database.py
@@ -94,30 +94,6 @@ def main():
         """
 
 
-
-        cd_ip = [
-            {"artist": "Deep Purple", "Title": "Made In Japan", "name": "Andy"},
-            {"artist": "Led Zeppelin", "Title": "House of the Holy", "name": "Andy"},
-            {"artist": "Pink Floyd", "Title": "DSOM", "name": "Andy"},
-            {"artist": "Albert Hammond", "Title": "Free Electric Band", "name": "Sam"},
-            {"artist": "Nilsson", "Title": "Without You", "name": "Sam"}
-        ]
-
-        result = cd.insert_many(cd_ip)
-
-        print_mdb_collection(cd)
-
-        # another collection
-        collector = db["collector"]
-
-        collector_ip = [
-            {"name": "Andy", "preference": "Rock"},
-            {"name": "Sam", "preference": "Pop"}
-        ]
-        result = collector.insert_many(collector_ip)
-
-        print_mdb_collection(collector)
-
         # related data
         for name in collector.find():
             print(f'List for {name["name"]}')
