c3eee6c886fa9b914839fc1d6e3415e3bd75234e
UncleanlyClericr <junyatwin@gmail.com>
2019-05-15 13:10:09 -0700

    Well, I'm certain if I get this None thing with rentals figured out, I'll be done.

--- students/jesse_miller/lesson07/assignment/src/parallel_db.py
+++ students/jesse_miller/lesson07/assignment/src/parallel_db.py
@@ -4,8 +4,11 @@ Lesson 07:  Linear DB loading
 # pylint: disable=C0103
 import csv
 import os
+import logging
 import time
+from pathlib import Path
 from timeit import timeit
+import threading
 from line_profiler import LineProfiler
 import pymongo
 
@@ -40,26 +43,27 @@ def _import_csv(filename):
     Returns a list of dictionaries.  One dictionary for each row of data in a
     csv file.
     '''
-
+    filename = Path.cwd().with_name('data') / filename
     with open(filename, newline='') as csvfile:
         dict_list = []
 
         csv_data = csv.reader(csvfile)
 
-        headers = next(csv_data, None)  # Save the first line as the headers
+        headers = next(csv_data, None)
 
-        if headers[0].startswith('ï»¿'):  # Check for weird formatting
+        if headers[0].startswith('ï»¿'):
             headers[0] = headers[0][3:]
 
         for row in csv_data:
-            row_dict = {column: row[index] for index, column in enumerate(headers)}
+            row_dict = {column: row[index] for index, column in \
+            enumerate(headers)}
 
             dict_list.append(row_dict)
 
         return dict_list
 
 
-def _add_bulk_data(collection, directory_name, filename):
+def _add_bulk_data(results, collection, directory_name, filename):
     '''
     Adds data in bulk to database.
     '''
@@ -74,70 +78,88 @@ def _add_bulk_data(collection, directory_name, filename):
     records_processed = final_records - initial_records
     run_time = time.time() - start_time
 
-    return records_processed, initial_records, final_records, run_time
+    stats = (records_processed, initial_records, final_records, run_time)
+
+    results[collection.name] = stats
 
 
-def import_data(d_base, directory_name, products_file, customers_file, rentals_file):
+def import_data(db, directory_name, products_file, customers_file, rentals_file):
     '''
     Takes a directory name and three csv files as input.  Creates and populates
     three collections in MongoDB.
     '''
 
-    products = d_base['products']
-    products_results = _add_bulk_data(products, directory_name, products_file)
+    products = db['products']
+    customers = db['customers']
+    rentals = db['rentals']
 
-    customers = d_base['customers']
-    customers_results = _add_bulk_data(customers, directory_name, customers_file)
+    results_dict = {}
 
-    rentals = d_base['rentals']
-    _add_bulk_data(rentals, directory_name, rentals_file)
+    threads = [threading.Thread(target=_add_bulk_data, args=(results_dict,
+                                                             products,
+                                                             directory_name,
+                                                             products_file)),
+               threading.Thread(target=_add_bulk_data, args=(results_dict,
+                                                             customers,
+                                                             directory_name,
+                                                             customers_file)),
+               threading.Thread(target=_add_bulk_data, args=(results_dict,
+                                                             rentals,
+                                                             directory_name,
+                                                             rentals_file))]
 
-    return [customers_results, products_results]
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    return [results_dict['customers'], results_dict['products'], results_dict['rentals']]
 
 
-def show_available_products(d_base):
+def show_available_products(db):
     '''
     Returns a dictionary for each product listed as available.
     '''
 
     available_products = {}
 
-    for product in d_base.products.find():
+    for product in db.products.find():
         if product['quantity_available'] != '0':
-            short_dict = {key: value for key, value in product.items() if key \
-            not in ('_id', 'product_id')}
-            available_products[product['product_id']] = short_dict
+            rental_users = {key: value for key, value in product.items() if \
+                          key not in ('_id', 'product_id')}
+            available_products[product['product_id']] = rental_users
 
     return available_products
 
 
-def show_rentals(d_base, product_id):
+def show_rentals(db, product_id):
     '''
     Returns a dictionary with user information from users who have rented
     products matching the product_id.
     '''
 
-    customer_info = {}
+    rental_users_dict = {}
 
-    for rental in d_base.rentals.find():
+    for rental in db.rentals.find():
         if rental['product_id'] == product_id:
             customer_id = rental['user_id']
-            customer_record = d_base.customers.find_one({'user_id': customer_id})
+            customer_record = db.customers.find_one({'user_id': customer_id})
 
-            short_dict = {key: value for key, value in customer_record.items() \
+            rental_users = {key: value for key, value in customer_record.items() \
             if key not in ('_id', 'user_id')}
-            customer_info[customer_id] = short_dict
+            rental_users_dict[customer_id] = rental_users
 
-    return customer_info
+    return rental_users_dict
 
 
-def clear_data(d_base):
+def clear_data(db):
     '''
     Delete data in MongoDB.
     '''
-    d_base.products.drop()
-    d_base.customers.drop()
-    d_base.rentals.drop()
+    db.products.drop()
+    db.customers.drop()
+    db.rentals.drop()
 
 
 def main():
@@ -147,11 +169,21 @@ def main():
     mongo = MongoDBConnection()
 
     with mongo:
-        d_base = mongo.connection.media
+        logging.info('Opening MongoDB')
+        db = mongo.connection.media
 
-        results = import_data(d_base, '', 'product.csv', 'customer.csv', 'rental.csv')
+        logging.info('Importing csv files')
+        results = import_data(db, '', 'product.csv', 'customer.csv',
+                              'rental.csv')
 
-        clear_data(d_base)
+        logging.info('Showing available products')
+        logging.info(show_available_products(db))
+
+        logging.info('\nShowing rental information for prd005')
+        logging.info(show_rentals(db, 'prd005'))
+
+        logging.info('\nClearing data from database.')
+        clear_data(db)
 
     return results
 
