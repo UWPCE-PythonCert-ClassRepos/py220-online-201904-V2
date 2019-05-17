47b5615a84c624db77c5fb48e26cf29071bb9356
Jeff Shabani <jeffrey.shabani@outlook.com>
2019-05-17 09:40:45 -0700

    lesson 7

--- students/jeff_shabani/lesson07/assignment/tests/test_performance.py
+++ students/jeff_shabani/lesson07/assignment/tests/test_performance.py
@@ -6,7 +6,7 @@ from linear import *
 from pathlib import Path
 
 current_path = Path.cwd()
-src_path = current_path.with_name('src')
+SRC_PATH = Path.cwd().with_name('data')
 
 
 class PerformanceTesting(unittest.TestCase):
@@ -20,7 +20,6 @@ class PerformanceTesting(unittest.TestCase):
         expected = (9999, 0, 9999)
         self.assertEqual(expected, import_data_threading()[0][:3])
         self.assertEqual(expected, import_data_threading()[1][:3])
-
 
     def test_parallel_record_process_type(self):
         """
@@ -47,7 +46,6 @@ class PerformanceTesting(unittest.TestCase):
         self.assertIsInstance(import_data_threading()[0][2], int)
         self.assertIsInstance(import_data_threading()[1][2], int)
 
-
     def test_parallel_import_process_time_type_two(self):
         """
         test process in 2nd result tuple is a float
@@ -56,6 +54,61 @@ class PerformanceTesting(unittest.TestCase):
         remove_a_collection()
         self.assertIsInstance(import_data_threading()[0][3], float)
         self.assertIsInstance(import_data_threading()[1][3], float)
+
+    def test_linear_process_result_list(self):
+        """
+        test content of each results list tuple
+        :return: tuple
+        """
+        remove_a_collection()
+        expected = (9999, 0, 9999)
+        # src_path = Path.cwd().with_name('data')
+        self.assertEqual(expected, import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[0][:3])
+        self.assertEqual(expected, import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[1][:3])
+
+    def test_linear_record_process_type(self):
+        """
+        Test that starting record processed counts are integers
+        :return: tuple
+        """
+        remove_a_collection()
+        self.assertIsInstance(import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[0][0], int)
+        self.assertIsInstance(import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[1][0], int)
+        
+    def test_linear_starting_record_count_type(self):
+        """
+        Test that starting record counts are integers
+        """
+        remove_a_collection()
+        self.assertIsInstance(import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[0][1], int)
+        self.assertIsInstance(import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[1][1], int)
+        
+    def test_linear_post_load_count_type(self):
+        """
+        Test that post-load record counts are integers
+        """
+        remove_a_collection()
+        self.assertIsInstance(import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[0][2], int)
+        self.assertIsInstance(import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[1][2], int)
+
+    def test_linear_import_process_time_type_two(self):
+        """
+        test process in 2nd result tuple is a float
+        :return: tuple
+        """
+        remove_a_collection()
+        self.assertIsInstance(import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[0][3], float)
+        self.assertIsInstance(import_data(SRC_PATH,
+                                               'product.csv', 'customers.csv')[1][3], float)
 
 
 if __name__ == '__main__':
