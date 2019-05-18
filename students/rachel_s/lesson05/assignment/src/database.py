98872cebce4dbd7a650b1fab39ddcc99e53b57a7
Rachel Schirra <schirra.rachel@gmail.com>
2019-05-18 15:55:58 -0700

    Managed to commit an old version, smooth move.

--- students/rachel_s/lesson05/assignment/src/database.py
+++ students/rachel_s/lesson05/assignment/src/database.py
@@ -202,28 +202,6 @@ def show_available_products():
     return avail
 
 
-'''
-show_available_products(): 
-Returns a Python dictionary of products listed as available with the
-following fields:
-
-product_id.
-description.
-product_type.
-quantity_available.
-
-For example:
-
-{‘prd001’:
-    {‘description’:‘60-inch TV stand’,
-    ’product_type’:’livingroom’,
-    ’quantity_available’:‘3’},
-    ’prd002’:{‘description’:’L-shaped sofa’,
-    ’product_type’:’livingroom’,
-    ’quantity_available’:‘1’}
-    }
-'''
-
 def show_rentals(product_id):
     '''
     Takes a product ID.
@@ -251,28 +229,3 @@ def show_rentals(product_id):
             records[item] = renter
     return records
 
-
-'''
-show_rentals(product_id): 
-
-Returns a Python dictionary with the following user information from
-users that have rented products matching product_id:
-
-user_id.
-name.
-address.
-phone_number.
-email.
-
-For example:
-
-{‘user001’:{‘name’:’Elisa Miles’,
-    ’address’:‘4490 Union Street’,
-    ’phone_number’:‘206-922-0882’,
-    ’email’:’elisa.miles@yahoo.com’},
-    ’user002’:{‘name’:’Maya Data’,
-    ’address’:‘4936 Elliot Avenue’,
-    ’phone_number’:‘206-777-1927’,
-    ’email’:’mdata@uw.edu’}
-}
-'''
