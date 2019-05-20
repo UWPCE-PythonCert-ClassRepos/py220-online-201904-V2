9d0be4fbaab81a02b151b3f8ce5100d3529cf33e
UncleanlyCleric <junyatwin@gmail.com>
2019-05-19 19:51:44 -0700

    Trying different things...

--- students/jesse_miller/lesson07/assignment/src/linear_db.py
+++ students/jesse_miller/lesson07/assignment/src/linear_db.py
@@ -6,10 +6,13 @@ Actually, turns out that's a lie.  This is a giant rabbit hole.
 '''
 import csv
 import logging
+# import time
+import os
 from timeit import timeit
 from pathlib import Path
 import pymongo
 from line_profiler import LineProfiler
+
 
 logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
@@ -31,6 +34,14 @@ class MongoDBConnection:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.close()
     logging.info('Database connected')
+
+
+def print_mdb_collection(collection_name):
+    '''
+    Prints all documents in a collection.
+    '''
+    for doc in collection_name.find():
+        print(doc)
 
 
 def _import_csv(filename):
@@ -55,37 +66,38 @@ def _import_csv(filename):
         return dict_list
 
 
-def import_data(db, directory_name, product_file, customers_file, rental_file):
+def _add_bulk_data(collection, directory_name, filename):
+    '''
+    Adds data in bulk to database.
+    '''
+    file_path = os.path.join(directory_name, filename)
+
+    # start_time = time.time()
+    initial_records = collection.count_documents({})
+
+    collection.insert_many(_import_csv(file_path), ordered=False)
+
+    final_records = collection.count_documents({})
+    records_processed = final_records - initial_records
+    # run_time = time.time() - start_time
+
+    return records_processed, initial_records, final_records #, run_time
+
+
+def import_data(db, directory_name, products_file, customers_file, rentals_file):
     '''
     Function to import data into three MongoDB tables
     '''
-    product_errors = 0
-    customer_errors = 0
-    rental_errors = 0
-    directory_name = directory_name
-    try:
-        products = db['products']
-        products.insert_many(_import_csv(product_file))
-    except ImportError:
-        product_errors += 1
-    try:
-        customers = db['customers']
-        customers.insert_many(_import_csv(customers_file))
-    except ImportError:
-        customer_errors += 1
-    try:
-        rentals = db['rentals']
-        rentals.insert_many(_import_csv(rental_file))
-    except ImportError:
-        rental_errors += 1
+    products = db['products']
+    products_results = _add_bulk_data(products, directory_name, products_file)
 
-    record_count = (db.products.count_documents({}),
-                    db.customers.count_documents({}),
-                    db.rentals.count_documents({}))
+    customers = db['customers']
+    customers_results = _add_bulk_data(customers, directory_name, customers_file)
 
-    error_count = (product_errors, customer_errors, rental_errors)
-    logging.info('Database Populated')
-    return record_count, error_count
+    rentals = db['rentals']
+    rentals_results = _add_bulk_data(rentals, directory_name, rentals_file)
+
+    return [products_results, customers_results, rentals_results]
 
 
 def show_available_products(db):
@@ -108,19 +120,22 @@ def show_rentals(db, product_id):
     Function to look up customers who have rented a specific product.
     '''
     rental_users_dict = {}
+
     for rental in db.rentals.find():
         if rental['product_id'] == product_id:
             customer_id = rental['user_id']
+            print(customer_id)
 
             customer_record = db.customers.find_one({'user_id': customer_id})
+            print(customer_record)
 
             rental_users = {'name': customer_record['name'],
                             'address': customer_record['address'],
                             'phone_number': customer_record['phone_number'],
                             'email': customer_record['email']}
-            rental_users_dict[customer_id] = rental_users
 
-    return rental_users_dict
+            rental_users_dict[customer_id] = rental_users
+        return rental_users_dict
 
 
 def clear_data(db):
@@ -146,9 +161,11 @@ def main():
         import_data(db, '', 'product.csv', 'customer.csv', 'rental.csv')
 
         logging.info('Showing available products')
+        logging.info(show_available_products(db))
 
-        logging.info('\nShowing rental information for prd005')
-        logging.info(show_rentals(db, 'prd005'))
+        logging.info('\nShowing rental information for P000003')
+        logging.info(show_rentals(db, 'P000003'))
+        print(show_rentals(db, 'P000004'))
 
         logging.info('\nClearing data from database.')
         clear_data(db)
