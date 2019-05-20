f3aa40466f3edfe50a796dc865abf8aa0a9baaaa
Jeff Shabani <jeffrey.shabani@outlook.com>
2019-05-20 11:35:21 -0700

    Lesson 8 work.

--- students/jeff_shabani/lesson08/assignment/src/inventory.py
+++ students/jeff_shabani/lesson08/assignment/src/inventory.py
@@ -1,4 +1,5 @@
-from collections import namedtuple
+"""
+Module that creates new inventory management objects"""
 from pathlib import Path
 import pandas as pd
 
@@ -12,11 +13,20 @@ SEED_DATA = [(None, 'Elisa Miles', 'LR04', 'Leather Sofa', 25.00),
              (None, 'Alex Gonzales', 'BR02', 'Queen Mattress', 17.00)]
 
 SEED_DATA_3 = [(None, '3Elisa Miles', 'LR04', 'Leather Sofa', 25.00),
-             (None, '3Edward Data', 'KT78', 'Kitchen Table', 10.00),
-             (None, '3Alex Gonzales', 'BR02', 'Queen Mattress', 17.00)]
+               (None, '3Edward Data', 'KT78', 'Kitchen Table', 10.00),
+               (None, '3Alex Gonzales', 'BR02', 'Queen Mattress', 17.00)]
 
 
 def add_furniture(file_name, source):
+    """
+    This function creates a dataframe from an input source. It then checks if
+    a with an input file name exists. If it is read in as a dataframe and the
+    new data is appended and the file is resaved with the new data. If the csv
+    does not previously exist it is created with the new data.
+    :param file_name:
+    :param source:
+    :return: new or modified csv file
+    """
     test = pd.DataFrame.from_records(source, columns=COLUMNS)
     test['invoice_file'] = file_name
     if check_file_exists(DATA_PATH / file_name):
@@ -29,9 +39,14 @@ def add_furniture(file_name, source):
 
 
 def check_file_exists(fpath_):
+    """
+    Function that checks if a file exists.\
+    :param fpath_:
+    :return: bool
+    """
     return Path.exists(fpath_)
 
 
-print(check_file_exists(DATA_PATH / 'inventory.csv'))
-
-add_furniture('inventory.csv', SEED_DATA)
+if __name__ == '__main__':
+    print(check_file_exists(DATA_PATH / 'inventory.csv'))
+    add_furniture('inventory.csv', SEED_DATA)
--- students/jeff_shabani/lesson08/assignment/tests/test_lesson_08.py
+++ students/jeff_shabani/lesson08/assignment/tests/test_lesson_08.py
@@ -1,125 +1,144 @@
 #!/usr/bin/env python3
 
 import os
+import pytest
 import unittest
 from inventory import *
-from linear import *
 from pathlib import Path
 
-current_path = Path.cwd()
-SRC_PATH = Path.cwd().with_name('data')
+
+DATA_PATH = Path.cwd().with_name('data')
+
+TEST_DATA=[(None, 'Elisa Miles', 'LR04', 'Leather Sofa', 25.00),
+            (None, 'Edward Data', 'KT78', 'Kitchen Table', 10.00),
+            (None, 'Alex Gonzales', 'BR02', 'Queen Mattress', 17.00)]
 
 
 class PerformanceTesting(unittest.TestCase):
 
-    def test_parallel_process_result_list(self):
+    def test_add_furniture(self):
         """
-        test content of each results list tuple
+        test that inventory csv does not exist and then tests
+        that it does exist after running adding records.
         :return: tuple
         """
-        remove_a_collection()
-        expected = (9999, 0, 9999)
-        self.assertEqual(expected, import_data_threading()[0][:3])
-        self.assertEqual(expected, import_data_threading()[1][:3])
+        self.assertFalse(False, Path.exists(DATA_PATH / 'inventory.csv'))
+        self.assertTrue(Path.exists(DATA_PATH / 'inventory.csv'),
+                        add_furniture('inventory.csv', TEST_DATA))
+        os.remove(DATA_PATH / 'inventory.csv')
 
 
-
-    def test_parallel_record_process_type(self):
+    def test_correct_number_of_records_added(self):
         """
-        Test that starting record processed counts are integers
+        tests that the correct number of records are
+        added to the inventory csv
         :return: tuple
         """
-        remove_a_collection()
-        self.assertIsInstance(import_data_threading()[0][0], int)
-        self.assertIsInstance(import_data_threading()[1][0], int)
+        add_furniture('inventory.csv', TEST_DATA)
+        test = pd.read_csv(DATA_PATH/'inventory.csv')
+        len_csv = int(test.iloc[:, 0].count())
+        self.assertEqual(3, len_csv)
+        os.remove(DATA_PATH / 'inventory.csv')
 
-
-    def test_parallel_starting_record_count_type(self):
-        """
-        Test that starting record counts are integers
-        """
-        remove_a_collection()
-        self.assertIsInstance(import_data_threading()[0][1], int)
-        self.assertIsInstance(import_data_threading()[1][1], int)
-
-
-    def test_parallel_post_load_count_type(self):
-        """
-        Test that post-load record counts are integers
-        """
-        remove_a_collection()
-        self.assertIsInstance(import_data_threading()[0][2], int)
-        self.assertIsInstance(import_data_threading()[1][2], int)
-
-
-    def test_parallel_import_process_time_type_two(self):
-        """
-        test process in 2nd result tuple is a float
-        :return: tuple
-        """
-        remove_a_collection()
-        self.assertIsInstance(import_data_threading()[0][3], float)
-        self.assertIsInstance(import_data_threading()[1][3], float)
-
-
-    def test_linear_process_result_list(self):
-        """
-        test content of each results list tuple
-        :return: tuple
-        """
-        remove_a_collection()
-        expected = (9999, 0, 9999)
-        # src_path = Path.cwd().with_name('data')
-        self.assertEqual(expected, import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[0][:3])
-        self.assertEqual(expected, import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[1][:3])
-
-
-    def test_linear_record_process_type(self):
-        """
-        Test that starting record processed counts are integers
-        :return: tuple
-        """
-        remove_a_collection()
-        self.assertIsInstance(import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[0][0], int)
-        self.assertIsInstance(import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[1][0], int)
-
-        
-    def test_linear_starting_record_count_type(self):
-        """
-        Test that starting record counts are integers
-        """
-        remove_a_collection()
-        self.assertIsInstance(import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[0][1], int)
-        self.assertIsInstance(import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[1][1], int)
-
-        
-    def test_linear_post_load_count_type(self):
-        """
-        Test that post-load record counts are integers
-        """
-        remove_a_collection()
-        self.assertIsInstance(import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[0][2], int)
-        self.assertIsInstance(import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[1][2], int)
-
-
-    def test_linear_import_process_time_type_two(self):
-        """
-        test process in 2nd result tuple is a float
-        :return: tuple
-        """
-        remove_a_collection()
-        self.assertIsInstance(import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[0][3], float)
-        self.assertIsInstance(import_data(SRC_PATH,
-                                               'product.csv', 'customers.csv')[1][3], float)
+#
+#
+#
+#     def test_parallel_record_process_type(self):
+#         """
+#         Test that starting record processed counts are integers
+#         :return: tuple
+#         """
+#         remove_a_collection()
+#         self.assertIsInstance(import_data_threading()[0][0], int)
+#         self.assertIsInstance(import_data_threading()[1][0], int)
+#
+#
+#     def test_parallel_starting_record_count_type(self):
+#         """
+#         Test that starting record counts are integers
+#         """
+#         remove_a_collection()
+#         self.assertIsInstance(import_data_threading()[0][1], int)
+#         self.assertIsInstance(import_data_threading()[1][1], int)
+#
+#
+#     def test_parallel_post_load_count_type(self):
+#         """
+#         Test that post-load record counts are integers
+#         """
+#         remove_a_collection()
+#         self.assertIsInstance(import_data_threading()[0][2], int)
+#         self.assertIsInstance(import_data_threading()[1][2], int)
+#
+#
+#     def test_parallel_import_process_time_type_two(self):
+#         """
+#         test process in 2nd result tuple is a float
+#         :return: tuple
+#         """
+#         remove_a_collection()
+#         self.assertIsInstance(import_data_threading()[0][3], float)
+#         self.assertIsInstance(import_data_threading()[1][3], float)
+#
+#
+#     def test_linear_process_result_list(self):
+#         """
+#         test content of each results list tuple
+#         :return: tuple
+#         """
+#         remove_a_collection()
+#         expected = (9999, 0, 9999)
+#         # src_path = Path.cwd().with_name('data')
+#         self.assertEqual(expected, import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[0][:3])
+#         self.assertEqual(expected, import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[1][:3])
+#
+#
+#     def test_linear_record_process_type(self):
+#         """
+#         Test that starting record processed counts are integers
+#         :return: tuple
+#         """
+#         remove_a_collection()
+#         self.assertIsInstance(import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[0][0], int)
+#         self.assertIsInstance(import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[1][0], int)
+#
+#
+#     def test_linear_starting_record_count_type(self):
+#         """
+#         Test that starting record counts are integers
+#         """
+#         remove_a_collection()
+#         self.assertIsInstance(import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[0][1], int)
+#         self.assertIsInstance(import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[1][1], int)
+#
+#
+#     def test_linear_post_load_count_type(self):
+#         """
+#         Test that post-load record counts are integers
+#         """
+#         remove_a_collection()
+#         self.assertIsInstance(import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[0][2], int)
+#         self.assertIsInstance(import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[1][2], int)
+#
+#
+#     def test_linear_import_process_time_type_two(self):
+#         """
+#         test process in 2nd result tuple is a float
+#         :return: tuple
+#         """
+#         remove_a_collection()
+#         self.assertIsInstance(import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[0][3], float)
+#         self.assertIsInstance(import_data(SRC_PATH,
+#                                                'product.csv', 'customers.csv')[1][3], float)
 
 
 if __name__ == '__main__':
