03f5979382ea47b90d79be5ac62ca62e53c44847
Douglas Klos <dougklos@gmail.com>
2019-05-21 06:08:04 -0700

    l08 setting up directory

---
+++ students/douglas_klos/lesson08/assignment/src/inventory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" HPNorton - Lesson 08 - Currying and Closures """
+
+
+def add_furniture(invoice_file, customer_name, item_code,
+                  item_description, item_monthly_rate):
+    pass
+
+
+def single_customer(customer_name, invoice_file):
+    pass
---
+++ students/douglas_klos/lesson08/assignment/tests/test_inventory.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Autograde Lesson 8 assignment """
+
+import pytest
+import inventory as l
+
+
+def test_add_furniture(invoice_file, customer_name, item_code,
+                       item_description, item_monthly_price):
+    pass
+
+
+def single_customer(customer_name, invoice_file):
+    pass
