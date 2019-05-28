90f5fbb9a9309873b1b268560dea1c1ed8c564cb
Rachel Schirra <schirra.rachel@gmail.com>
2019-05-27 22:17:12 -0700

    Added add_furniture function

--- students/rachel_s/lesson08/assignment/src/inventory.py
+++ students/rachel_s/lesson08/assignment/src/inventory.py
@@ -6,33 +6,12 @@ Lesson 08
 Functions to add furniture to a CSV file.
 '''
 
+from pathlib import Path
+
 # Rachel Schirra
 # May 27, 2019
 # Python 220 Lesson 08
 
-
-'''
-Create a function called add_furniture that takes the following input
-parameters:
-
-invoice_file
-customer_name
-item_code
-item_description
-item_monthly_price
-
-This function will create invoice_file (to replace the spreadsheet’s
-data) if it doesn’t exist or append a new line to it if it does. After
-adding a few items to the same file, the file created by add_furniture
-should look something like this:
-
-Elisa Miles,LR04,Leather Sofa,25.00
-Edward Data,KT78,Kitchen Table,10.00
-Alex Gonzales,BR02,Queen Mattress,17.00
-
-You can create a starter file in this format for testing, or you can
-have your add function do it.
-'''
 
 def add_furniture(
         invoice_file,
@@ -45,7 +24,18 @@ def add_furniture(
     Adds an item to the file invoice_file. If the file specified does
     not exist, it creates the file and then adds the item.
     '''
-    pass
+    my_inv = Path(invoice_file)
+    if my_inv.is_file():
+        my_file = open(my_inv, 'a')
+    else:
+        my_file = open(my_inv, 'w')
+    my_file.write(','.join([
+        customer_name,
+        item_code,
+        item_description,
+        item_monthly_price
+        ]))
+    my_file.close()
 
 
 '''
@@ -66,4 +56,3 @@ def single_customer(customer_name, invoice_file):
     function iterates through rental_items and adds each item to
     invoice_file.
     '''
-
