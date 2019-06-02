518b8bb346e5ed457f219b1e9abb6cf2d9821528
Jeremy <jereamon@gmail.com>
2019-06-02 10:03:20 -0700

    Final commit before assignment 09 submission.

--- students/jeremy_monroe/lesson09/assignment/src/database.py
+++ students/jeremy_monroe/lesson09/assignment/src/database.py
@@ -1,4 +1,13 @@
-""" The main file for Jeremy Monroe Assignment 05 """
+"""
+Jeremy Monroe assignment 05 revisited for assignment 09
+
+I didn't add all that much to the exisiting context manager. Just a few
+additional class variables that allowed for easier access to database
+collections.
+
+It was interesting
+to revisit this with the new understanding of context managers though!
+"""
 
 import logging
 import csv
@@ -11,26 +20,32 @@ logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 
-class MongoDatabaseConnection():# {{{
+class MongoDatabaseConnection():  # {{{
     """ MongoDATABAE Connection """
 
     def __init__(self, host="127.0.0.1", port=27017):
         self.host = host
         self.port = port
         self.connection = None
+        self.product_collection = None
+        self.customer_collection = None
+        self.rental_collection = None
 
     def __enter__(self):
         self.connection = MongoClient(self.host, self.port)
+        self.product_collection = self.connection.assignment_05['product']
+        self.customer_collection = self.connection.assignment_05['customer']
+        self.rental_collection = self.connection.assignment_05['rental']
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.connection.close()# }}}
+        self.connection.close()  # }}}
 
 
 MONGO = MongoDatabaseConnection()
 
 
-def import_data(directory_name, product_file, customer_file, rentals_file):# {{{
+def import_data(directory_name, product_file, customer_file, rentals_file):
     """
     Takes a dir name and three csv files. It populates a database with the data
     provdided and returns information about said data within two tuples.
@@ -49,43 +64,52 @@ def import_data(directory_name, product_file, customer_file, rentals_file):# {{{
 
     try:
         with MONGO:
-            DATABAE = MONGO.connection.assignment_05
-
-            product_collection = DATABAE["product"]
             for product in parsed_product_file:
-                product_collection.insert_one(product)
+                try:
+                    MONGO.product_collection.insert_one(product)
+                except TypeError as excep:
+                    LOGGER.info(
+                        "Error saving product info to database: %s", excep)
+                    product_errors += 1
+                else:
+                    product_count += 1
 
     except TypeError as excep:
-        LOGGER.info("Error saving product info to database: %s", excep)
-        product_errors = 1
+        LOGGER.warning("Critical error, no products saved to database")
     else:
         LOGGER.info("Successfully saved product info")
         product_count = len(parsed_product_file)
 
     try:
         with MONGO:
-            DATABAE = MONGO.connection.assignment_05
-            customer_collection = DATABAE["customer"]
-
             for customer in parsed_customer_file:
-                customer_collection.insert_one(customer)
+                try:
+                    MONGO.customer_collection.insert_one(customer)
+                except TypeError as excep:
+                    LOGGER.info(
+                        "Error saving customer info to database: %s", excep)
+                    customer_errors += 1
+                else:
+                    customer_count += 1
     except TypeError as excep:
-        LOGGER.info("Error saving customer info to database: %s", excep)
-        customer_errors = 1
+        LOGGER.warning("Critical error, no customers saved to database!")
     else:
         LOGGER.info("Successfully saved customer info.")
         customer_count = len(parsed_customer_file)
 
     try:
         with MONGO:
-            DATABAE = MONGO.connection.assignment_05
-            rental_collection = DATABAE["rental"]
-
             for rental in parsed_rentals_file:
-                rental_collection.insert_one(rental)
+                try:
+                    MONGO.rental_collection.insert_one(rental)
+                except TypeError as excep:
+                    LOGGER.info(
+                        "Error saving rental info to database: %s", excep)
+                    rental_errors += 1
+                else:
+                    rental_count += 1
     except TypeError as excep:
-        LOGGER.info("Error saving rental info to database: %s", excep)
-        rental_errors = 1
+        LOGGER.info("Critical error, no rentals saved to database!")
     else:
         LOGGER.info("Successfully saved rental info.")
         rental_count = len(parsed_rentals_file)
@@ -93,10 +117,10 @@ def import_data(directory_name, product_file, customer_file, rentals_file):# {{{
     return (
         (product_count, customer_count, rental_count),
         (product_errors, customer_errors, rental_errors),
-    )# }}}
+    )
 
 
-def parse_csv_input(input_file):# {{{
+def parse_csv_input(input_file):  # {{{
     """
     Parses and returns an input csv file as a list of lists.
     """
@@ -128,18 +152,17 @@ def parse_csv_input(input_file):# {{{
 
         return temp_object_storage
     except FileNotFoundError as excep:
-        LOGGER.info("error parsing csv file: %s", excep)# }}}
+        LOGGER.info("error parsing csv file: %s", excep)  # }}}
 
 
-def show_available_products():# {{{
+def show_available_products():  # {{{
     """
     Returns a dict containing information about products listed as available.
     """
     products_available = {}
     try:
         with MONGO:
-            product_collection = (MONGO.connection
-                                  .assignment_05["product"].find())
+            product_collection = (MONGO.product_collection.find())
 
             for product in product_collection:
                 if int(product["quantity_available"]) > 0:
@@ -156,10 +179,10 @@ def show_available_products():# {{{
             LOGGER.info('No products found')
         else:
             LOGGER.info("Available products retrieved successfully.")
-            return products_available# }}}
+            return products_available  # }}}
 
 
-def show_rentals(product_id):# {{{
+def show_rentals(product_id):  # {{{
     """
     Returns a dict containing information about customers who have rented a
     product based on the provided product_id.
@@ -167,8 +190,8 @@ def show_rentals(product_id):# {{{
     cust_rent_dict = {}
     try:
         with MONGO:
-            DATABAE = MONGO.connection.assignment_05
-            customer_rental = DATABAE.rental.aggregate(
+            # DATABAE = MONGO.connection.assignment_05
+            customer_rental = MONGO.rental_collection.aggregate(
                 [
                     {
                         "$lookup": {
@@ -202,7 +225,7 @@ def show_rentals(product_id):# {{{
             LOGGER.info("Product: %s not found.", product_id)
         else:
             LOGGER.info('Retrieved rental info for product: %s', product_id)
-            return cust_rent_dict# }}}
+            return cust_rent_dict  # }}}
 
 
 if __name__ == "__main__":
--- students/jeremy_monroe/lesson09/assignment/src/jpgdiscover.py
+++ students/jeremy_monroe/lesson09/assignment/src/jpgdiscover.py
@@ -1,4 +1,15 @@
-""" Jeremy Monroe Assignment 09 """
+"""
+Jeremy Monroe Assignment 09
+
+I didn't quite meet the requirements for this one. I do use recursion to
+analyze a list of files in order to determine their filetype. Unfortunately the
+returned results do not meet the desired returned results.
+
+In short I end up with lists within lists rather than just a list...
+
+If you run this module directly from within the src directory the results will
+be printed and you can see what I mean.
+"""
 
 import os
 import argparse
@@ -9,15 +20,78 @@ def parse_cmd_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filetype', nargs='?', const=1,
                         help=("Enter the filetype to"
-                              " search for. Ex: '.jpg'"), default='.jpg')
+                              " search for. Ex: '.jpg'"), default='.png')
 
     return parser.parse_args()
 
 
 def list_jpg_files(dir_name):
     """
-    Takes a directory name and finds all jpg files within said directory and
-    any subdirectories.
+    Ok, list_jpg_files uses os.walk and a provided dir_name (directory name).
+    The results are then passed into loop_directories and subsequently
+    find_jpg_files to be processed in a recursive fashion.
+    """
+    files_and_dirs = list(os.walk(dir_name))
+    # print(files_and_dirs)
+
+    return loop_directories(files_and_dirs)
+
+
+def loop_directories(files_and_dirs):
+    """
+    Takes a list of lists returned from os.walk, loops over those lists and
+    passes relevant information (list of filenames) on to find_jpg_files for
+    further analyzation.
+    """
+    jpg_dir_list = []
+    # print("at top of loop directories")
+    for directory in files_and_dirs:
+        found_jpgs = [x for x in find_jpg_files(directory[2]) if x]
+
+        if found_jpgs:
+            jpg_dir_list.append(directory[0])
+            jpg_dir_list.append(found_jpgs)
+
+    return jpg_dir_list
+
+
+def find_jpg_files(possible_files):
+    """
+    I wanted to implement recursive calls in loop_directories and here but
+    wound up struggling quite a bit just to get it working here.
+
+    Anyways, this takes a list and recursively analyzes its items to determine
+    whether or not they ought to be returned.
+
+    I never got this working in a way that will pass the provided tests. It
+    does work, and does return consistent results, but I ran into a return issue
+    where if the final else block is reached and '' is returned I end up with
+    lists within lists, rather than the expected single list.
+
+    I might clean that issue up after the fact when the list is returned to
+    loop_directories.
+    """
+    # print("Top of find_jpg_files possible_files = {}".format(possible_files))
+    if possible_files:
+        if '.png' in possible_files[-1]:
+            return [find_jpg_files(possible_files[:-1]), possible_files[-1]]
+        else:
+            return find_jpg_files(possible_files[:-1])
+    else:
+        return ''
+
+
+def better_list_jpg_files(dir_name):
+    """
+    Does the same thing as list_jpg_files only it uses argparser to specify the
+    file type to search for.
+
+    ONLY works when __name__ == __main__
+
+    This is me quite a bit later. This doesn't really do the same thing as
+    list_jpg_files anymore. The results are similar but this one outputs what I
+    was hoping to achieve with list_jpg_files. Unfortunately this function isn't
+    recursive!!!
     """
     jpg_list = []
 
@@ -35,6 +109,8 @@ def list_jpg_files(dir_name):
 
 if __name__ == '__main__':
     FILETYPE = parse_cmd_arguments().filetype
-    print(list_jpg_files('../data/'))
 
-    # print(f"Printing jpg_list {jpg_list}")
+    # print(better_list_jpg_files('../data/'))
+
+    print(list_jpg_files('../data/'))
+    # list_jpg_files('../data/')
