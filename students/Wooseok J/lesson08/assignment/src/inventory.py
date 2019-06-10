8844f682b9b25c83e59d9add6e4f0b84f434a6cd
woosukjeung <47073569+woosukjeung@users.noreply.github.com>
2019-06-10 22:31:03 +0900

    updated lesson 8 for better pylint score

--- students/Wooseok J/lesson08/assignment/src/inventory.py
+++ students/Wooseok J/lesson08/assignment/src/inventory.py
@@ -1,12 +1,13 @@
+import csv
+
+
 def add_furniture(invoice_file, customer_name, item_code,
                   item_description, item_monthly_price):
     with open(invoice_file, 'a',  newline='') as file:
-        file.write('{:},{:},{:},{:.2f}\n'.format(customer_name,
-                                                 item_code,
-                                                 item_description,
-                                                 item_monthly_price))
-    return (invoice_file, customer_name, item_code,
-            item_description, item_monthly_price)
+        writer = csv.writer(file)
+        data = customer_name, item_code, item_description, item_monthly_price
+        writer.writerow(data)
+    return customer_name, item_code, item_description, item_monthly_price
 
 
 def single_customer(customer_name, invoice_file):
@@ -24,12 +25,11 @@ def single_customer(customer_name, invoice_file):
 
 
 if __name__ == "__main__":
-    PATH = "../data/"
-    add_furniture(PATH + "rented_items.csv", "Elisa Miles",
+    add_furniture("rented_items.csv", "Elisa Miles",
                   "LR04", "Leather Sofa", 25.00)
-    add_furniture(PATH + "rented_items.csv", "Edward Data",
+    add_furniture("rented_items.csv", "Edward Data",
                   "KT78", "Kitchen Table", 10.00)
-    add_furniture(PATH + "rented_items.csv", "Alex Gonzales",
+    add_furniture("rented_items.csv", "Alex Gonzales",
                   "BR02", "Queen Mattress", 17.00)
-    CREATE_INVOICE = single_customer("Susan Wong", PATH + "rented_items.csv")
-    CREATE_INVOICE(PATH + "test_items.csv")
+    CREATE_INVOICE = single_customer("Susan Wong", "rented_items.csv")
+    CREATE_INVOICE("test_items.csv")
