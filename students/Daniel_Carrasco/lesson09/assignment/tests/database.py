30d5a70a531942176fb2dc46bfd355992f3d1634
dac2229 <dac2229@gmail.com>
2019-06-09 17:50:00 -0700

    adding database and test, no real changes but learned about context manager

---
+++ students/Daniel_Carrasco/lesson09/assignment/tests/database.py
@@ -0,0 +1,212 @@
+""""
+Lesson 5. Create DB using mongodb api
+"""
+
+# pylint: disable= W1203, R0914, C0103, W0703, W0612
+import logging
+import csv
+import os
+from pymongo import MongoClient  # high level api
+
+
+LOG_FORMAT = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+FORMATTER = logging.Formatter(LOG_FORMAT)
+
+FILE_HANDLER = logging.FileHandler('db.log')
+FILE_HANDLER.setLevel(logging.INFO)
+FILE_HANDLER.setFormatter(FORMATTER)
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(FILE_HANDLER)
+
+
+class MongoDBConnection():
+    """
+    Class to start MongoDB Connection
+    """
+
+    def __init__(self, host='127.0.0.1', port=27017):
+        """ be sure to use the ip address not name for local windows"""
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = MongoClient(self.host, self.port)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+
+def import_data(directory_name, product_file, customer_file, rentals_file):
+    """
+    method to import the csv files that will be added to the db
+    """
+    LOGGER.info("starting MongoDBConnection")
+    mongo = MongoDBConnection()
+
+    with mongo:
+
+        # mongodb database; it all starts here
+        db = mongo.connection.HPNorton
+
+        # collection in database
+        products = db["products"]
+        customers = db["customers"]
+        rentals = db["rentals"]
+        db.products.drop()
+        db.customers.drop()
+        db.rentals.drop()
+
+        LOGGER.info("importing data")
+        product_ip = read_data(directory_name, product_file)
+        customer_ip = read_data(directory_name, customer_file)
+        rentals_ip = read_data(directory_name, rentals_file)
+
+        product_results = add_many_ip(products, product_ip)
+        customer_results = add_many_ip(customers, customer_ip)
+        rental_results = add_many_ip(rentals, rentals_ip)
+
+    import_count = (
+        db.products.count_documents({}),
+        db.customers.count_documents({}),
+        db.rentals.count_documents({})
+    )
+
+    LOGGER.info(f'succesful product imports = {import_count[0]} to db')
+    LOGGER.info(f'succesful customer imports = {import_count[1]} to db')
+    LOGGER.info(f'succesful rental imports = {import_count[2]} to db')
+
+    error_count = (product_results, customer_results, rental_results)
+    LOGGER.info(f'product import errors = {error_count[0]} to db')
+    LOGGER.info(f'customer import errors = {error_count[1]} to db')
+    LOGGER.info(f'rental import errors = {error_count[2]} to db')
+
+    return import_count, error_count
+
+
+def read_data(directory_name, file_name):
+    """
+    method to read in the csv files
+    """
+    LOGGER.info(f'reading {file_name} data from {directory_name}')
+    ip_list = []
+
+    try:
+        with open(directory_name + file_name) as csv_file:
+            reader = csv.reader(csv_file)
+            header = next(reader, None)
+            header[0] = header[0].replace("\ufeff", "")
+
+            for row in reader:
+                temp_dict = {}
+                for index, value in enumerate(header):
+                    temp_dict[value] = row[index]
+                ip_list.append(temp_dict)
+        LOGGER.info("successfully read in data")
+
+    except Exception as error:
+        LOGGER.info(f'could not read data due to {error}')
+
+    return ip_list
+
+
+def add_many_ip(collection_name, collection_ip):
+    """
+    method to add the data to the collection
+    """
+
+    try:
+        collection_name.insert_many(collection_ip)
+        LOGGER.info(f'no errors importing to {collection_name} ')
+        error = 0
+        return error
+    except Exception as error:
+        LOGGER.info(f'add_many_ip error of {error} for to {collection_name}')
+        error = 1
+        return error
+
+
+def show_available_products():
+    """
+    Method to show available products
+    """
+    mongo = MongoDBConnection()
+    LOGGER.info("starting show_available_products method")
+    with mongo:
+        # mongodb database; it all starts here
+        db = mongo.connection.HPNorton
+        avail_products_dict = {}
+        query = {'quantity_available': {'$gt': '1'}}
+        for query_results in db.products.find(query):
+            key = query_results["product_id"]
+            values = {
+                "description": query_results["description"],
+                "product_type": query_results["product_type"],
+                "quantity_available": query_results["quantity_available"]
+            }
+            temp_dict = {key: values}
+            avail_products_dict.update(temp_dict)
+    LOGGER.info(f'available products = {avail_products_dict}')
+    return avail_products_dict
+
+
+def show_rentals(product_id):
+    """
+    Method to show available products
+    """
+    mongo = MongoDBConnection()
+    LOGGER.info("starting show_rentals method")
+    with mongo:
+        # mongodb database; it all starts here
+        db = mongo.connection.HPNorton
+        show_rentals_dict = {}
+        query = {'product_id': product_id}
+        for query_results in db.rentals.find(query):
+            query_2 = {'user_id': query_results['user_id']}
+            for query_results_2 in db.customers.find(query_2):
+                key = query_results_2['user_id']
+                value = {
+                    'name': query_results_2['name'],
+                    'address': query_results_2['address'],
+                    'phone_number': query_results_2['phone_number'],
+                    'email': query_results_2['email']
+                }
+                temp_dict = {key: value}
+                show_rentals_dict.update(temp_dict)
+    LOGGER.info(
+        f'showing rentals that match "{product_id}" = {show_rentals_dict}')
+
+    return show_rentals_dict
+
+
+def drop_data():
+    """
+    method to drop the data from db
+    """
+    mongo = MongoDBConnection()
+    LOGGER.info("starting drop data method")
+    with mongo:
+        # mongodb database; it all starts here
+        db = mongo.connection.HPNorton
+        db.products.drop()
+        db.customers.drop()
+        db.rentals.drop()
+
+
+def main():
+    """
+    main used to call other methods
+    """
+    cwd = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    import_count, error_count = import_data(
+        cwd+"/", "product.csv", "customers.csv", "rental.csv")
+    show_available_products()
+    show_rentals("prd002")
+    drop_data()
+
+
+if __name__ == "__main__":
+    main()
--- students/Daniel_Carrasco/lesson09/assignment/tests/test_database.py
+++ students/Daniel_Carrasco/lesson09/assignment/tests/test_database.py
@@ -1,39 +1,55 @@
-import pytest
-import os
+#!/usr/bin/env python3
+# pylint: disable = E1101
+"""
+grade lesson 5
+"""
 
+import os
+import pytest
+import sys
 import database as l
 
 @pytest.fixture
 def _show_available_products():
-    return {
-            'P000001':{'description':'Chair Red leather','product_type':'livingroom','quantity_available':'21'},
-            'P000002':{'description':'Table Oak','product_type':'livingroom','quantity_available':'4'},
-            'P000003':{'description':'Couch Green cloth','product_type':'livingroom','quantity_available':'10'},
-            'P000004':{'description':'Dining table Plastic','product_type':'Kitchen','quantity_available':'23'},
-            'P000005':{'description':'Stool Black ash','product_type':'Kitchen','quantity_available':'12'}
-           }    
+    return{
+        'prd001': {'description': '60-inch TV stand', 'product_type': 'livingroom', 'quantity_available': '3'},
+        'prd003': {'description': 'Acacia kitchen table', 'product_type': 'kitchen', 'quantity_available': '7'},
+        'prd004': {'description': 'Queen bed', 'product_type': 'bedroom', 'quantity_available': '10'},
+        'prd005': {'description': 'Reading lamp', 'product_type': 'bedroom', 'quantity_available': '20'},
+        'prd006': {'description': 'Portable heater', 'product_type': 'bathroom', 'quantity_available': '14'},
+        'prd008': {'description': 'Smart microwave', 'product_type': 'kitchen', 'quantity_available': '30'},
+        'prd010': {'description': '60-inch TV', 'product_type': 'livingroom', 'quantity_available': '3'}
+        }
 
 @pytest.fixture
 def _show_rentals():
     return {
-            'C000001':{'name':'Shea Boehm','address':'3343 Sallie Gateway','phone_number':'508.104.0644','email':'Alexander.Weber@monroe.com'},
-            'C000003':{'name':'Elfrieda Skiles','address':'3180 Mose Row','phone_number':'839)825-0058','email':'Mylene_Smitham@hannah.co.uk'}
-           }
+            'user008': {'name': 'Shirlene Harris', 'address': '4329 Honeysuckle Lane', 'phone_number': '206-279-5340', 'email': 'harrisfamily@gmail.com'},
+            'user005': {'name': 'Dan Sounders', 'address': '861 Honeysuckle Lane', 'phone_number': '206-279-1723', 'email': 'soundersoccer@mls.com'}
+            }
+
 
 def test_import_data():
-    dir = os.path.dirname(os.path.abspath(__file__))
-    added, errors = l.import_data(dir, "products.csv", "customers.csv", "rentals.csv")
+    """ import """
+    cwd = os.path.abspath(os.path.join(os.path.dirname( __file__ )))
+    print(cwd)
+    added, errors = l.import_data(cwd+"/", "product.csv", "customers.csv", "rental.csv")
+    print(added)
     for add in added:
         assert isinstance(add, int)
+
     for error in errors:
-        assert isinstance(error, int)  
-    assert added == (5,11,9)
-    assert errors == (0,0,0)
+        assert isinstance(error, int)
+
+    assert added == (10, 10, 9)
+    assert errors == (0, 0, 0)
 
 def test_show_available_products(_show_available_products):
+    """ available products """
     students_response = l.show_available_products()
     assert students_response == _show_available_products
 
 def test_show_rentals(_show_rentals):
-    students_response = l.show_rentals("P000003")
+    """ rentals """
+    students_response = l.show_rentals("prd002")
     assert students_response == _show_rentals
